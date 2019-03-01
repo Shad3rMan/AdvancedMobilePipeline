a7ea778d6a5d0618b5f65f141087aa79ea0c241a
Andrew Chaiko <andrewc@playtika.com>
2019-03-01 19:37:01 +0300

    modifying cull mode

--- Assets/MobilePipeline/Shaders/Editor/SuperShaderEditor.cs
+++ Assets/MobilePipeline/Shaders/Editor/SuperShaderEditor.cs
@@ -12,15 +12,15 @@ namespace MobilePipeline.Shaders.Editor
             Opaque,
             Transparent
         }
-        
+
         private MaterialEditor _materialEditor;
         private MaterialProperty _blendMode;
         private MaterialProperty _albedoMap;
         private MaterialProperty _albedoColor;
+        private MaterialProperty _cullMode;
 
         public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] props)
         {
-            base.OnGUI(materialEditor, props);
             FindProperties(props);
             _materialEditor = materialEditor;
             var material = materialEditor.target as Material;
@@ -30,14 +30,14 @@ namespace MobilePipeline.Shaders.Editor
                 {
                     SetupMaterialWithBlendMode(material, (BlendMode) material.GetFloat(Styles.Lit));
                     BlendModePopup();
+                    CullModePopup();
                     DoAlbedoArea(material);
+                }
+                if (EditorGUI.EndChangeCheck())
+                {
                 }
             }
 
-            if (EditorGUI.EndChangeCheck())
-            {
-            }
-            
             EditorGUILayout.Space();
             _materialEditor.EnableInstancingField();
             _materialEditor.DoubleSidedGIField();
@@ -51,14 +51,30 @@ namespace MobilePipeline.Shaders.Editor
         private void BlendModePopup()
         {
             EditorGUI.showMixedValue = _blendMode.hasMixedValue;
-            var mode = (BlendMode)_blendMode.floatValue;
+            var mode = (BlendMode) _blendMode.floatValue;
 
             EditorGUI.BeginChangeCheck();
-            mode = (BlendMode)EditorGUILayout.Popup(Styles.RenderingMode, (int)mode, Styles.BlendNames);
+            mode = (BlendMode) EditorGUILayout.Popup(Styles.RenderingMode, (int) mode, Styles.BlendNames);
             if (EditorGUI.EndChangeCheck())
             {
                 _materialEditor.RegisterPropertyChangeUndo("Rendering Mode");
-                _blendMode.floatValue = (float)mode;
+                _blendMode.floatValue = (float) mode;
+            }
+
+            EditorGUI.showMixedValue = false;
+        }
+
+        private void CullModePopup()
+        {
+            EditorGUI.showMixedValue = _cullMode.hasMixedValue;
+            var mode = (CullMode) _cullMode.floatValue;
+
+            EditorGUI.BeginChangeCheck();
+            mode = (CullMode) EditorGUILayout.Popup(Styles.CullMode, (int) mode, Styles.CullNames);
+            if (EditorGUI.EndChangeCheck())
+            {
+                _materialEditor.RegisterPropertyChangeUndo("Cull Mode");
+                _cullMode.floatValue = (float) mode;
             }
 
             EditorGUI.showMixedValue = false;
@@ -69,6 +85,7 @@ namespace MobilePipeline.Shaders.Editor
             _blendMode = FindProperty("_Lit", props);
             _albedoMap = FindProperty("_MainTex", props);
             _albedoColor = FindProperty("_Color", props);
+            _cullMode = FindProperty("_Cull", props);
         }
 
         private static void SetupMaterialWithBlendMode(Material material, BlendMode blendMode)
@@ -77,30 +94,36 @@ namespace MobilePipeline.Shaders.Editor
             {
                 case BlendMode.Opaque:
                     material.SetOverrideTag("RenderType", "Geometry");
-                    material.SetInt(Styles.SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
-                    material.SetInt(Styles.DstBlend, (int)UnityEngine.Rendering.BlendMode.Zero);
+                    material.SetInt(Styles.SrcBlend, (int) UnityEngine.Rendering.BlendMode.One);
+                    material.SetInt(Styles.DstBlend, (int) UnityEngine.Rendering.BlendMode.Zero);
                     material.SetInt(Styles.ZWrite, 1);
-                    material.renderQueue = (int)RenderQueue.Geometry;
+                    material.renderQueue = (int) RenderQueue.Geometry;
                     break;
                 case BlendMode.Transparent:
                     material.SetOverrideTag("RenderType", "Transparent");
-                    material.SetInt(Styles.SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
-                    material.SetInt(Styles.DstBlend, (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    material.SetInt(Styles.SrcBlend, (int) UnityEngine.Rendering.BlendMode.One);
+                    material.SetInt(Styles.DstBlend, (int) UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
                     material.SetInt(Styles.ZWrite, 1);
-                    material.renderQueue = (int)RenderQueue.Transparent;
+                    material.renderQueue = (int) RenderQueue.Transparent;
                     break;
             }
         }
-        
+
         private static class Styles
         {
+            public static readonly int Cull = Shader.PropertyToID("_Cull");
             public static readonly int Lit = Shader.PropertyToID("_Lit");
             public static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
             public static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
             public static readonly int ZWrite = Shader.PropertyToID("_ZWrite");
-            public static readonly GUIContent AlbedoText = EditorGUIUtility.TrTextContent("Albedo", "Albedo (RGB) and Transparency (A)");
+
+            public static readonly GUIContent AlbedoText =
+                EditorGUIUtility.TrTextContent("Albedo", "Albedo (RGB) and Transparency (A)");
+
             public static readonly string[] BlendNames = Enum.GetNames(typeof(BlendMode));
+            public static readonly string[] CullNames = Enum.GetNames(typeof(CullMode));
             public const string RenderingMode = "Rendering Mode";
+            public const string CullMode = "Cull Mode";
         }
     }
 }
--- Assets/MobilePipeline/Shaders/SuperLit.shader
+++ Assets/MobilePipeline/Shaders/SuperLit.shader
@@ -2,9 +2,9 @@
 {
     Properties
     {
-        _Color ("Color", Color) = (1, 1, 1, 1)
-        _MainTex("Albedo & Alpha", 2D) = "white" {}
-        [Enum(UnityEngine.Rendering.CullMode)] _Cull ("Cull", Float) = 2
+        [HideInInspector]_Color ("Color", Color) = (1, 1, 1, 1)
+        [HideInInspector]_MainTex("Albedo & Alpha", 2D) = "white" {}
+        [HideInInspector] _Cull ("Cull", Float) = 2
 		[HideInInspector] _Lit ("Lit", Float) = 1
     }
     SubShader
